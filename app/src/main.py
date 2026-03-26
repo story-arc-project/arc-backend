79b35669652cb7107731c0b96e9514a0251cce1f
Bunniesnu <bunniesnu@gmail.com>
2026-03-26 23:55:31 +0900

    Fix deprecated APIs

--- app/src/main.py
+++ app/src/main.py
@@ -1,9 +1,11 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from src.db import engine
 from src.models import Base
 
-app = FastAPI()
-
-@app.on_event("startup")
-def startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     Base.metadata.create_all(bind=engine)
+    yield
+
+app = FastAPI(lifespan=lifespan)
