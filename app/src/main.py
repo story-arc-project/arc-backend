dd679fbdc9fdb7da89c6ea6fe40fb3b81cb3aaf6
Bunniesnu <bunniesnu@gmail.com>
2026-04-01 20:29:20 +0900

    Add CORS with .env configuration

--- app/src/main.py
+++ app/src/main.py
@@ -1,5 +1,7 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from os import getenv
 from src.db.db import create_db_and_tables
 from src.api.auth import auth_router
 
@@ -9,6 +11,16 @@ async def lifespan(app: FastAPI):  # pyright: ignore[reportUnusedParameter]
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+origins = getenv("FRONTEND_HOSTS", "").split(",")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(
     auth_router,
