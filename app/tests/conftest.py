f0fd35eaf31ad6a704275c5f52a09e749979ac79
Bunniesnu <bunniesnu@gmail.com>
2026-04-02 13:54:47 +0900

    Add pytest for mail sending automations

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 from fakeredis import FakeRedis
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 @pytest.fixture(autouse=True)
 def fake_redis():
@@ -10,5 +10,21 @@ def fake_redis():
         yield fake
 
 @pytest.fixture(autouse=True)
-def set_jwt_key(monkeypatch: pytest.MonkeyPatch):
-    monkeypatch.setenv("JWT_KEY", "testsecrettestsecrettestsecrettestsecret")
+def mock_mail():
+    with patch("src.utils.mail.smtplib.SMTP") as mock_smtp, \
+        patch("src.utils.mail.getenv") as mock_getenv:
+
+        def fake_env(key: str):
+            return {
+                "GMAIL": "test@gmail.com",
+                "GMAIL_PASSWORD": "password"
+            }.get(key)
+
+        mock_getenv.side_effect = fake_env
+
+        mock_server = MagicMock()
+        mock_smtp.return_value.__enter__.return_value = mock_server
+
+        mock_server.send_message.return_value = {}
+
+        yield mock_server
--- app/tests/test_auth.py
+++ app/tests/test_auth.py
@@ -2,11 +2,14 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlmodel import Session, SQLModel, create_engine, select
 from sqlmodel.pool import StaticPool
+from unittest.mock import MagicMock
+from email.mime.multipart import MIMEMultipart
 
 from src.db.models import User
 from src.enums import UserStatus
 from src.main import app
 from src.db.db import get_session
+from src.utils.mail import send_mail
 
 
 # Test data
@@ -33,6 +36,38 @@ def client(session: Session):
     app.dependency_overrides[get_session] = override
     yield TestClient(app)
     app.dependency_overrides.clear()
+
+
+def get_sent_mail(mock_mail: MagicMock):
+    sent_mail: MIMEMultipart = mock_mail.send_message.call_args[0][0]
+    for part in sent_mail.walk():
+        if part.get_content_type() == "text/plain" and not part.is_multipart():
+            return {
+                "To": sent_mail["To"],
+                "Subject": sent_mail["Subject"],
+                "Body": part.get_payload(decode=True).decode(
+                    part.get_content_charset() or "utf-8"
+                )
+            }
+    return {
+        "To": sent_mail["To"],
+        "Subject": sent_mail["Subject"],
+        "Body": None
+    }
+
+
+def test_send_mail(mock_mail: MagicMock):
+    to = "senttest@gmail.com"
+    subject = "hi"
+    body = "hi"
+
+    _ = send_mail(to, subject, body)
+    mock_mail.send_message.assert_called_once()
+
+    sent_mail = get_sent_mail(mock_mail)
+    assert sent_mail["To"] == to
+    assert sent_mail["Subject"] == subject
+    assert sent_mail["Body"] == body
 
 
 def test_signup(client: TestClient):
