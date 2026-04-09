8f7592147006fb2951a6c7f6f7e74241280ede87
Bunniesnu <bunniesnu@gmail.com>
2026-04-09 21:17:48 +0900

    Add comprehensive analysis

--- ai_analyst/src/main.py
+++ ai_analyst/src/main.py
@@ -1,11 +1,16 @@
 from typing import Annotated
-from src.individual import main
+from src.individual import main as individual_main
+from src.comprehensive import main as comprehensive_main
 from fastapi import Body, FastAPI
 
-from src.models import IndividualRequest
+from src.models import ComprehensiveRequest, IndividualRequest
 
 app = FastAPI()
 
 @app.post("/individual")
 async def individual(body: Annotated[IndividualRequest, Body()]):
-    return main(body.input)
+    return individual_main(body.input)
+
+@app.post("/comprehensive")
+async def comprehensive(body: Annotated[ComprehensiveRequest, Body()]):
+    return comprehensive_main(body.input, body.school, body.department)
--- ai_analyst/src/models.py
+++ ai_analyst/src/models.py
@@ -3,3 +3,9 @@ from pydantic import BaseModel
 
 class IndividualRequest(BaseModel):
     input: list[str]
+
+
+class ComprehensiveRequest(BaseModel):
+    input: list[str]
+    school: str
+    department: str
