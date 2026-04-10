c89a03e4d6347f161742e4d5a7be3a6f90f56db8
Bunniesnu <bunniesnu@gmail.com>
2026-04-10 18:43:44 +0900

    Edit to remove tokens only when token related exception response

--- app/src/main.py
+++ app/src/main.py
@@ -8,6 +8,7 @@ from fastapi.responses import JSONResponse
 from src.api.models.exc import AppException
 from src.db.db import create_db_and_tables
 from src.api.auth import auth_router, remove_tokens
+from src.enums import ErrorResponseCode
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):  # pyright: ignore[reportUnusedParameter]
@@ -32,7 +33,7 @@ async def app_exception_handler(request: Request, exc: AppException):
         status_code = exc.status_code,
         content = exc.error.model_dump()
     )
-    if response.status_code == 403:
+    if response.status_code == 403 and exc.error.code in [ErrorResponseCode.AUTH_REVOKED, ErrorResponseCode.AUTH_REUSE_DETECTED]:
         remove_tokens(response)
     return response
 
