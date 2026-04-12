5ecaeef2d1f25282b5749aeb6e126b51963526a5
Bunniesnu <bunniesnu@gmail.com>
2026-04-12 13:13:13 +0900

    Add endpoint /presets/:id DELETE (not tested)

--- app/src/api/presets.py
+++ app/src/api/presets.py
@@ -5,6 +5,7 @@ from sqlmodel import select
 
 from src.api.models.base import ErrorResponse, PresetResponseData, PresetsResponseData, SuccessResponseWithData
 from src.api.models.exc import AppException
+from src.api.models.response import DeleteSuccessResponse
 from src.db.db import SessionDep
 from src.db.models import Preset
 from src.enums import ErrorResponseCode
@@ -50,4 +51,41 @@ async def get_preset_by_id(preset_id: UUID, session: SessionDep, response: Respo
     return SuccessResponseWithData[PresetResponseData](
         message = "Fetch success",
         data = PresetResponseData(**result.model_dump())
+    )
+
+@presets_router.delete("/{preset_id}")
+async def delete_preset(preset_id: UUID, session: SessionDep, response: Response, payload: Annotated[AccessTokenPayload, Depends(check_auth)]):
+    statement = select(Preset).where(Preset.id == preset_id)
+    result = session.exec(statement).one_or_none()
+    if result is None:
+        raise AppException(
+            404,
+            ErrorResponse(
+                code = ErrorResponseCode.NOT_FOUND,
+                message = "Preset not found"
+            )
+        )
+    if result.user_id != payload.sub:
+        raise AppException(
+            403,
+            ErrorResponse(
+                code = ErrorResponseCode.RESOURCE_NOT_ALLOWED,
+                message = "Access for the resource is not allowed"
+            )
+        )
+    try:
+        session.delete(result)
+        session.commit()
+    except:
+        session.rollback()
+        raise AppException(
+            500,
+            ErrorResponse(
+                code=ErrorResponseCode.SERVER_ERROR,
+                message="Server side error."
+            )
+        )
+    response.status_code = 204
+    return DeleteSuccessResponse(
+        message = "Preset deleted."
     )
