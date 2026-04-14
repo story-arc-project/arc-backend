5cf63d37a3176738d301cf9ac0973e6b529b2d49
Bunniesnu <bunniesnu@gmail.com>
2026-04-14 19:01:16 +0900

    Fix docker compose dependencies

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ cd "$(dirname "$0")" || exit 1
 source ./config.sh
 docker compose -f ./init/docker-compose.yml -p "$PROJECT_NAME" --project-directory . up --exit-code-from certbot
 docker compose -f ./init/docker-compose.yml -p "$PROJECT_NAME" --project-directory . down
-docker build -t arc-backend-app ./app
+docker compose build
 docker compose -f ./docker-compose.yml -p "$PROJECT_NAME" up -d
