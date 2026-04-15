5598bcb42a2dcff68d62011db1df4e750b055aeb
Bunniesnu <bunniesnu@gmail.com>
2026-04-15 19:37:37 +0900

    Use fake internal secret key on tests

--- ai_analyst/tests/test_signature.py
+++ ai_analyst/tests/test_signature.py
@@ -3,32 +3,38 @@
 import hashlib
 import hmac
 import json
-from os import getenv
 from typing import Any
+from unittest.mock import patch
 from uuid import uuid4
 
 from src.queue.tasks import sign_body
 
 
 INTERNAL_SECRET_KEY = "INTERNAL_SECRET"
+INTERNAL_SECRET_FAKE = "internalkeyinternalkeyinternalkey"
 
 def verify_signature(body: dict[str, Any], signature: str):
-    key = getenv(INTERNAL_SECRET_KEY)
-    if key is None:
-        raise ValueError("Internal secret not set.")
     expected = hmac.new(
-        key.encode(),
+        INTERNAL_SECRET_FAKE.encode(),
         json.dumps(body, separators=(",", ":")).encode(),
         hashlib.sha256
     ).hexdigest()
     return hmac.compare_digest(expected, signature)
 
 def test_signature():
-    analysis_id = str(uuid4())
-    data = {"summary": "test result"}
-    body = {
-        "analysis_id": analysis_id,
-        "result": data
-    }
-    signature= sign_body(body)
-    assert verify_signature(body, signature)
+    with patch("src.queue.tasks.getenv") as mock_getenv:
+        def fake_env(key: str):
+            return {
+                INTERNAL_SECRET_KEY: INTERNAL_SECRET_FAKE
+            }.get(key)
+
+        mock_getenv.side_effect = fake_env
+
+        analysis_id = str(uuid4())
+        data = {"summary": "test result"}
+        body = {
+            "analysis_id": analysis_id,
+            "result": data
+        }
+        signature= sign_body(body)
+        assert verify_signature(body, signature)
