3a6055c05f349b99d9067ed4d6b5dc85090dc30d
Bunniesnu <bunniesnu@gmail.com>
2026-05-20 22:33:56 +0900

    Add keyword internal route

--- app/src/api/internal.py
+++ app/src/api/internal.py
@@ -7,7 +7,7 @@ from fastapi import APIRouter, Depends, Response
 from src.api.models.base import ErrorResponse
 from src.api.models.exc import AppException
 from src.db.db import SessionDep
-from src.db.models import ComprehensiveAnalysis, IndividualAnalysis
+from src.db.models import ComprehensiveAnalysis, IndividualAnalysis, KeywordAnalysis
 from src.enums import AnalysisStatus, ErrorResponseCode
 from src.utils.internal import check_internal
 
@@ -68,6 +68,36 @@ async def success_comprehensive(body: Annotated[dict[str, Any], Depends(check_in
         )
     analysis_result: dict[str, Any] = json.loads(result)
     analysis.vector = analysis_result.get("vector")
+    analysis.result = analysis_result.get("result")
+    analysis.status = AnalysisStatus.SUCCESS
+    session.add(analysis)
+    session.commit()
+    response.status_code = 200
+    return {}
+
+@internal_router.post("/keyword/success")
+async def success_keyword(body: Annotated[dict[str, Any], Depends(check_internal)], session: SessionDep, response: Response):
+    analysis_id: str | None = body.get("analysis_id")
+    result: str | None = body.get("result")
+    if analysis_id is None or result is None:
+        raise AppException(
+            status_code = 400,
+            error = ErrorResponse(
+                code = ErrorResponseCode.BAD_REQUEST,
+                message = ""
+            )
+        )
+    statement = select(KeywordAnalysis).where(KeywordAnalysis.id == UUID(analysis_id))
+    analysis = session.exec(statement).one_or_none()
+    if analysis is None:
+        raise AppException(
+            status_code = 404,
+            error = ErrorResponse(
+                code = ErrorResponseCode.NOT_FOUND,
+                message = ""
+            )
+        )
+    analysis_result: dict[str, Any] = json.loads(result)
     analysis.result = analysis_result.get("result")
     analysis.status = AnalysisStatus.SUCCESS
     session.add(analysis)
