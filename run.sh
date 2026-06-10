bc91ae29ef44e4dbabee5ce7a6a87157948c73a3
Bunniesnu <bunniesnu@gmail.com>
2026-06-11 01:17:46 +0900

    Fix run.sh Build before compose down

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 cd "$(dirname "$0")" || exit 1
-./stop.sh
 source ./config.sh
+docker compose build
+docker compose -f ./docker-compose.yml -p "$PROJECT_NAME" down
 docker compose -f ./init/docker-compose.yml -p "$PROJECT_NAME" --project-directory . up --exit-code-from certbot
 docker compose -f ./init/docker-compose.yml -p "$PROJECT_NAME" --project-directory . down
-docker compose build
 docker compose -f ./docker-compose.yml -p "$PROJECT_NAME" up -d
