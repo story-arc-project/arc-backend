52a0bc511894a67a0be250e0ecc59f5650babf7b
Bunniesnu <bunniesnu@gmail.com>
2026-06-15 17:22:15 +0900

    Add compose bake to docker compose build

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 set -e
 cd "$(dirname "$0")" || exit 1
 source ./config.sh
-docker compose build
+COMPOSE_BAKE=true docker compose -f ./docker-compose.yml -p "$PROJECT_NAME" build
 docker compose -f ./docker-compose.yml -p "$PROJECT_NAME" down
 docker compose -f ./init/docker-compose.yml -p "$PROJECT_NAME" --project-directory . up --exit-code-from certbot
 docker compose -f ./init/docker-compose.yml -p "$PROJECT_NAME" --project-directory . down
