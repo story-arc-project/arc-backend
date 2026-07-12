f474745f0db5ee5103b43d3b04d5ffc1a6f89f36
Bunniesnu <bunniesnu@gmail.com>
2026-07-12 18:20:30 +0900

    BAC-13 Add unauthenticated, missing env tests to admin tests

--- app/tests/test_admin.py
+++ app/tests/test_admin.py
@@ -5,7 +5,8 @@ from os import getenv
 from tests.const import AUTHENTICATED_EMAIL
 
 class TestAuthAccess:
-    def test_auth_access_fail(self, authenticated_client: TestClient):
+    def test_auth_access_fail(self, authenticated_client: TestClient, monkeypatch: MonkeyPatch):
+        monkeypatch.setenv("ADMIN_EMAILS", "a" + AUTHENTICATED_EMAIL)
         response = authenticated_client.get("/admin/status")
         assert response.status_code == 404
         assert response.text == '{"detail":"Not Found"}'
@@ -15,3 +16,16 @@ class TestAuthAccess:
         assert getenv("ADMIN_EMAILS") == AUTHENTICATED_EMAIL
         response = authenticated_client.get("/admin/status")
         assert response.status_code == 200
+    
+    def test_auth_access_unauthenticated(self, client: TestClient, monkeypatch: MonkeyPatch):
+        monkeypatch.setenv("ADMIN_EMAILS", AUTHENTICATED_EMAIL)
+        assert getenv("ADMIN_EMAILS") == AUTHENTICATED_EMAIL
+        response = client.get("/admin/status")
+        assert response.status_code == 404
+        assert response.text == '{"detail":"Not Found"}'
+    
+    def test_auth_access_missing_env(self, authenticated_client: TestClient, monkeypatch: MonkeyPatch):
+        monkeypatch.delenv("ADMIN_EMAILS", raising=False)
+        response = authenticated_client.get("/admin/status")
+        assert response.status_code == 404
+        assert response.text == '{"detail":"Not Found"}'
