a780ada55c1e2f7c011b1d1afe7fd27f67f69b82
Bunniesnu <bunniesnu@gmail.com>
2026-07-19 00:57:04 +0900

    Fix pydantic validation on check_internal

--- app/src/api/internal.py
+++ app/src/api/internal.py
@@ -13,15 +13,16 @@ from src.utils.internal import check_internal
 internal_router = APIRouter()
 
 @internal_router.post("/{analysis_type}/failure")
-async def fail_individual(analysis_type: str, body: Annotated[InternalRequestFailure, Depends(check_internal)], session: SessionDep, response: Response):
+async def fail_individual(analysis_type: str, body: Annotated[dict, Depends(check_internal)], session: SessionDep, response: Response):
+    body_validated = InternalRequestFailure.model_validate(body)
     if analysis_type == "individual":
-        statement = select(IndividualAnalysis).where(IndividualAnalysis.id == body.analysis_id)
+        statement = select(IndividualAnalysis).where(IndividualAnalysis.id == body_validated.analysis_id)
     elif analysis_type == "comprehensive":
-        statement = select(ComprehensiveAnalysis).where(ComprehensiveAnalysis.id == body.analysis_id)
+        statement = select(ComprehensiveAnalysis).where(ComprehensiveAnalysis.id == body_validated.analysis_id)
     elif analysis_type == "keyword":
-        statement = select(KeywordAnalysis).where(KeywordAnalysis.id == body.analysis_id)
+        statement = select(KeywordAnalysis).where(KeywordAnalysis.id == body_validated.analysis_id)
     elif analysis_type == "resume":
-        statement = select(Resume).where(Resume.id == body.analysis_id)
+        statement = select(Resume).where(Resume.id == body_validated.analysis_id)
     else:
         raise AppException(
             status_code = 400,
@@ -46,8 +47,9 @@ async def fail_individual(analysis_type: str, body: Annotated[InternalRequestFai
     return {}
 
 @internal_router.post("/individual/success")
-async def success_individual(body: Annotated[InternalRequestSuccess, Depends(check_internal)], session: SessionDep, response: Response):
-    statement = select(IndividualAnalysis).where(IndividualAnalysis.id == body.analysis_id)
+async def success_individual(body: Annotated[dict, Depends(check_internal)], session: SessionDep, response: Response):
+    body_validated = InternalRequestSuccess.model_validate(body)
+    statement = select(IndividualAnalysis).where(IndividualAnalysis.id == body_validated.analysis_id)
     analysis = session.exec(statement).one_or_none()
     if analysis is None:
         raise AppException(
@@ -57,7 +59,7 @@ async def success_individual(body: Annotated[InternalRequestSuccess, Depends(che
                 message = ""
             )
         )
-    if body.vector is None:
+    if body_validated.vector is None:
         raise AppException(
             status_code = 400,
             error = ErrorResponse(
@@ -65,8 +67,8 @@ async def success_individual(body: Annotated[InternalRequestSuccess, Depends(che
                 message = ""
             )
         )
-    analysis.vector = body.vector
-    analysis.result = body.result
+    analysis.vector = body_validated.vector
+    analysis.result = body_validated.result
     analysis.status = AnalysisStatus.SUCCESS
     session.add(analysis)
     session.commit()
@@ -74,8 +76,9 @@ async def success_individual(body: Annotated[InternalRequestSuccess, Depends(che
     return {}
 
 @internal_router.post("/comprehensive/success")
-async def success_comprehensive(body: Annotated[InternalRequestSuccess, Depends(check_internal)], session: SessionDep, response: Response):
-    statement = select(ComprehensiveAnalysis).where(ComprehensiveAnalysis.id == body.analysis_id)
+async def success_comprehensive(body: Annotated[dict, Depends(check_internal)], session: SessionDep, response: Response):
+    body_validated = InternalRequestSuccess.model_validate(body)
+    statement = select(ComprehensiveAnalysis).where(ComprehensiveAnalysis.id == body_validated.analysis_id)
     analysis = session.exec(statement).one_or_none()
     if analysis is None:
         raise AppException(
@@ -85,7 +88,7 @@ async def success_comprehensive(body: Annotated[InternalRequestSuccess, Depends(
                 message = ""
             )
         )
-    if body.vector is None:
+    if body_validated.vector is None:
         raise AppException(
             status_code = 400,
             error = ErrorResponse(
@@ -93,8 +96,8 @@ async def success_comprehensive(body: Annotated[InternalRequestSuccess, Depends(
                 message = ""
             )
         )
-    analysis.vector = body.vector
-    analysis.result = body.result
+    analysis.vector = body_validated.vector
+    analysis.result = body_validated.result
     analysis.status = AnalysisStatus.SUCCESS
     session.add(analysis)
     session.commit()
@@ -102,8 +105,9 @@ async def success_comprehensive(body: Annotated[InternalRequestSuccess, Depends(
     return {}
 
 @internal_router.post("/keyword/success")
-async def success_keyword(body: Annotated[InternalRequestSuccess, Depends(check_internal)], session: SessionDep, response: Response):
-    statement = select(KeywordAnalysis).where(KeywordAnalysis.id == body.analysis_id)
+async def success_keyword(body: Annotated[dict, Depends(check_internal)], session: SessionDep, response: Response):
+    body_validated = InternalRequestSuccess.model_validate(body)
+    statement = select(KeywordAnalysis).where(KeywordAnalysis.id == body_validated.analysis_id)
     analysis = session.exec(statement).one_or_none()
     if analysis is None:
         raise AppException(
@@ -113,7 +117,7 @@ async def success_keyword(body: Annotated[InternalRequestSuccess, Depends(check_
                 message = ""
             )
         )
-    analysis.result = body.result
+    analysis.result = body_validated.result
     analysis.status = AnalysisStatus.SUCCESS
     session.add(analysis)
     session.commit()
@@ -121,8 +125,9 @@ async def success_keyword(body: Annotated[InternalRequestSuccess, Depends(check_
     return {}
 
 @internal_router.post("/resume/success")
-async def success_resume(body: Annotated[InternalRequestSuccess, Depends(check_internal)], session: SessionDep, response: Response):
-    statement = select(Resume).where(Resume.id == body.analysis_id)
+async def success_resume(body: Annotated[dict, Depends(check_internal)], session: SessionDep, response: Response):
+    body_validated = InternalRequestSuccess.model_validate(body)
+    statement = select(Resume).where(Resume.id == body_validated.analysis_id)
     resume = session.exec(statement).one_or_none()
     if resume is None:
         raise AppException(
@@ -132,7 +137,7 @@ async def success_resume(body: Annotated[InternalRequestSuccess, Depends(check_i
                 message = ""
             )
         )
-    resume.result = body.result
+    resume.result = body_validated.result
     session.add(resume)
     session.commit()
     response.status_code = 200
