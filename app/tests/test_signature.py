8d9f888f58e55fcabe4ef569b9bb163651a2ce9d
Bunniesnu <bunniesnu@gmail.com>
2026-08-04 17:02:01 +0900

    Fix test_signature.py errors

--- app/tests/test_signature.py
+++ app/tests/test_signature.py
@@ -11,7 +11,7 @@ INTERNAL_SECRET_KEY = "INTERNAL_SECRET"
 INTERNAL_SECRET_FAKE = "internalkeyinternalkeyinternalkey"
 
 def sign_body(body: dict[str, Any]):
-    body_bytes = json.dumps(body, separators=(",", ":")).encode()
+    body_bytes = json.dumps(body, separators=(",", ":"), sort_keys=True, default=str).encode()
     return hmac.new(INTERNAL_SECRET_FAKE.encode(), body_bytes, hashlib.sha256).hexdigest()
 
 def test_call_frontend(client: TestClient):
@@ -23,20 +23,17 @@ def test_call_frontend(client: TestClient):
 
         mock_getenv.side_effect = fake_env
 
-        body = {'analysis_id': 'e358547e-1adb-450a-b0a4-49cbc910ea68', 'result': {'summary': 'test result'}}
+        body = {'analysis_id': 'e358547e-1adb-450a-b0a4-49cbc910ea68', 'result': {'summary': 'test result', 'schema_version': 'individual/v1.0'}}
         signature = sign_body(body)
-        assert signature == "fce5f812c96a58d2b890e42e95f7e0618db8de7a187aace10b8b02537fc1ec0a"
         res = client.post(
             f"/{getenv("INTERNAL_ROUTE", "internal")}/individual/success",
             json=body,
-            headers={"X-Signature": signature + "2"},
-            timeout=10
+            headers={"X-Signature": signature + "2"}
         )
         assert res.status_code == 403
         res = client.post(
             f"/{getenv("INTERNAL_ROUTE", "internal")}/individual/success",
             json=body,
-            headers={"X-Signature": signature},
-            timeout=10
+            headers={"X-Signature": signature}
         )
         assert res.status_code == 404
