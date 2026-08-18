21f7797b3c30522064e42d098e8d204cfa5d003a
Bunniesnu <bunniesnu@gmail.com>
2026-08-18 14:42:24 +0900

    Add mailgun support for sending mail

--- app/src/utils/mail.py
+++ app/src/utils/mail.py
@@ -17,13 +17,13 @@ def send_mail(to: EmailStr, subject: str, text_body: str, html_body: str):
         raise ValueError("Gmail address is not set.")
 
     msg = MIMEMultipart("alternative")
-    msg["From"] = email
+    msg["From"] = f"ARC <{email}>"
     msg["To"] = to
     msg["Subject"] = subject
     msg.attach(MIMEText(text_body, "plain"))
     msg.attach(MIMEText(html_body, "html"))
 
-    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+    with smtplib.SMTP("smtp.mailgun.org", 587) as server:
         _ = server.starttls()
         _ = server.login(email, get_password())
         errors = server.send_message(msg)
